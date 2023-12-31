e4546217cf632a01c3bd2063ace214aff981ecd8
Jonathon Chew <>
2023-12-31 21:24:02 +0000

    Updated and confirmed working after some youtube code changes

--- JavaScript/Remove-All-Youtube-Watch-Later-Videos.js
+++ JavaScript/Remove-All-Youtube-Watch-Later-Videos.js
@@ -1,8 +1,15 @@
 //paste into console log
-setInterval(function() {
+/* setInterval(function() {
 	document.querySelector('#contents button[aria-label="Action menu"]').click();
 	var things = document.evaluate('//span[contains(text(),"Watch later")]',document,null,XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,null);
 		for (var i = 0; i < things.snapshotLength; i++) {
 		    things.snapshotItem(i).click();
 		}		
-}, 1000);
+}, 1000);*/
+
+//paste into console log
+setInterval(function() {
+	document.querySelector('#contents button[aria-label="Action menu"]').click();
+	var things = document.querySelector("#items > ytd-menu-service-item-renderer:nth-child(3) > tp-yt-paper-item")
+	things.snapshotItem(i).click();
+	}, 1000);
