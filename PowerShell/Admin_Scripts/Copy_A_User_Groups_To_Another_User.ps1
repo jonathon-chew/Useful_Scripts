d063dbd9050ee89ab1e9710cbe6084f7a12ad769
Jonathon Chew <>
2024-01-21 21:49:58 +0000

    Removed what looked like unneeded declaration of code $group=$group

--- PowerShell/Admin_Scripts/Copy_A_User_Groups_To_Another_User.ps1
+++ PowerShell/Admin_Scripts/Copy_A_User_Groups_To_Another_User.ps1
@@ -1,6 +1,10 @@
-function copyUserGroups(){$firstUser= Read-host "Whose groups would you like to copy? (Username) "
-$secondUser = Read-Host "Who would you like to copy the groups to? (Username) "
-$creds = Get-Credential
-$groups=$groups=(Get-ADUser $firstUser -Properties MemberOf).memberof | Get-ADGroup | Select-Object name
+function copyUserGroups(){
+    $firstUser= Read-host "Whose groups would you like to copy? (Username) "
+    $secondUser = Read-Host "Who would you like to copy the groups to? (Username) "
+    $creds = Get-Credential
+    $groups=(Get-ADUser $firstUser -Properties MemberOf).memberof | Get-ADGroup | Select-Object name
 
-foreach ($group in $groups) { Add-ADGroupMember -identity $group.name -Members $secondUser -Credential $creds}}
+foreach ($group in $groups) { 
+    Add-ADGroupMember -identity $group.name -Members $secondUser -Credential $creds
+    }
+}
