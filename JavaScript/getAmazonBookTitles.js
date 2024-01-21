525e23398ff06baf4381f5813c13abce3079f7c4
Jonathon Chew <>
2024-01-21 21:07:26 +0000

    Book Titles on Kindle

---
+++ JavaScript/getAmazonBookTitles.js
@@ -0,0 +1,16 @@
+let allBookTitles = []
+
+for (let index = 1; index < 15; index++) {
+  
+    window.location.href = `https://www.amazon.co.uk/hz/mycd/digital-console/contentlist/booksPurchases/dateDsc?pageNumber=${index}`
+
+    let bookTitles = document.querySelectorAll('.digital_entity_title')
+
+  for (let i = 0; i < bookTitles.length; i++) {
+    let bookTitle = bookTitles[i];
+
+		allBookTitles.push(bookTitle.innerHTML)
+
+  }
+  
+}
