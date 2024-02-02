5db5efed0998cd40ea95e3935596f8ecea1ccca9
Jonathon Chew <>
2024-02-02 20:26:22 +0000

    Updating the file to the new screenshot location, pretty changes and removing some lines to streamline

--- Python/Computer Tasks/File_Tidy/Picture_Tidy/Tidy_Up_Photos.py
+++ Python/Computer Tasks/File_Tidy/Picture_Tidy/Tidy_Up_Photos.py
@@ -1,15 +1,15 @@
-#!/Library/Frameworks/Python.framework/Versions/3.11/bin/python3
+#!/opt/homebrew/bin/python3
+
 import os
 import os.path
 
-path = "/Users/hunteradder626/Pictures"
+path = "/Users/hunteradder626/Pictures/Screenshots"
 fileNames = []
 screenshots = []
 
 def get_the_date(seperatedFileName):
     for date in screenshots:
         fileDate = seperatedFileName[1].split("-")
-        print(fileDate)
         return fileDate
         
 for i in os.listdir(path):
@@ -19,17 +19,20 @@ for i in os.listdir(path):
 
 for eachFile in fileNames: 
     seperatedFileName = eachFile.split() 
-    # print(seperatedFileName[0])
+
     if seperatedFileName[0] == "Screenshot":
+        
         screenshots.append(eachFile)
         get_the_date(seperatedFileName)
         fileDate = get_the_date(seperatedFileName)
-        yearFolder = path + "/" + fileDate[0]
-        monthDict = {1:'1. Jan', 2:'2. Feb', 3:'3. Mar', 4:'4. Apr', 5:'5. May', 6:'6. Jun', 
+        
+        yearFolder  = path + "/" + fileDate[0]
+        monthDict   = {1:'1. Jan', 2:'2. Feb', 3:'3. Mar', 4:'4. Apr', 5:'5. May', 6:'6. Jun', 
             7:'7. Jul', 8:'8. Aug', 9:'9. Sep', 10:'10. Oct', 11:'11. Nov', 12:'12. Dec'}
-        month = monthDict[int(fileDate[1])]
+        month       = monthDict[int(fileDate[1])]
         monthFolder = yearFolder + "/" + month
-        dayFolder = monthFolder  + "/" + fileDate[2]
+        dayFolder   = monthFolder  + "/" + fileDate[2]
+
         try:
             if not os.path.exists(yearFolder):
                 os.mkdir(yearFolder)
@@ -37,12 +40,16 @@ for eachFile in fileNames:
                 os.mkdir(monthFolder)
             if not os.path.exists(dayFolder):
                 os.mkdir(dayFolder)
+        
         except OSError:
             print ("Creation of the directory %s failed" % dayFolder)
+
         else:
+
             print ("Successfully created the directory %s " % dayFolder)
             print("Files ready to be moved")
-        source = path + "/" + eachFile
+        
+        source      = path + "/" + eachFile
         destination = dayFolder + "/" + eachFile 
         os.rename(source, destination)
         print(f"File {eachFile} moved to {destination}")    
